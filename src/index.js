caff5c7e1eaf40b0c1e307429bf4069c8440ef71
k-fish <kevan.fisher@sentry.io>
2020-06-16 07:22:41 -0700

    Local sentry

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as Sentry from '@sentry/browser';
+import { Integrations as ApmIntegrations } from '@sentry/apm';
+
+Sentry.init({
+  dsn: 'http://6e416ae2cdef4f56aeeabd91924898b9@localhost:8000/2',
+  integrations: [
+    new ApmIntegrations.Tracing(),
+  ],
+  tracesSampleRate: 0.99,
+});
+
+const timeout = (wait) => new Promise(resolve => setTimeout(resolve, wait));
+
+const fakeTracingFunction = async () => {
+  const transaction = Sentry.startTransaction({name: 'test-transaction'});
+  const span = transaction.startChild({op: 'functionX'}); // This function returns a Span
+
+  await timeout(2000);
+
+  span.finish(); // Remember that only finished spans will be sent with the transaction
+  transaction.finish(); // Finishing the transaction will send it to Sentry
+};
+
+fakeTracingFunction();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
