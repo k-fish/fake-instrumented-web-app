b58566bb2d83bb5f8d307a32766011ef85f1113a
Kev <fisherk.work@gmail.com>
2025-08-13 17:57:07 -0400

    Add localStorage persistence and Clear button for extraData

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./App.css";
 
 import { logger } from "@sentry/browser";
@@ -12,7 +12,14 @@ correlationId=1234567890`;
 
 /* eslint-disable no-undef */
 function App() {
-  const [extraData, setExtraData] = useState([{ key: "", value: "" }]);
+  const [extraData, setExtraData] = useState(() => {
+    const saved = localStorage.getItem('extraData');
+    return saved ? JSON.parse(saved) : [{ key: "", value: "" }];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('extraData', JSON.stringify(extraData));
+  }, [extraData]);
 
   function callMethodThatDoesntExist() {
     const methodName = "capture";
@@ -67,10 +74,15 @@ function App() {
     setExtraData(updated);
   }
 
+  function clearAllData() {
+    setExtraData([{ key: "", value: "" }]);
+    localStorage.removeItem('extraData');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <svg className="App-logo pixelated" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 66" width="400" height="367">
+        <svg className="App-logo pixelated" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 66" width="150" height="150">
           <path d="M29,2.26a4.67,4.67,0,0,0-8,0L14.42,13.53A32.21,32.21,0,0,1,32.17,40.19H27.55A27.68,27.68,0,0,0,12.09,17.47L6,28a15.92,15.92,0,0,1,9.23,12.17H4.62A.76.76,0,0,1,4,39.06l2.94-5a10.74,10.74,0,0,0-3.36-1.9l-2.91,5a4.54,4.54,0,0,0,1.69,6.24A4.66,4.66,0,0,0,4.62,44H19.15a19.4,19.4,0,0,0-8-17.31l2.31-4A23.87,23.87,0,0,1,23.76,44H36.07a35.88,35.88,0,0,0-16.41-31.8l4.67-8a.77.77,0,0,1,1.05-.27c.53.29,20.29,34.77,20.66,35.17a.76.76,0,0,1-.68,1.13H40.6q.09,1.91,0,3.81h4.78A4.59,4.59,0,0,0,50,39.43a4.49,4.49,0,0,0-.62-2.28Z" transform="translate(11, 11)" fill="#362d59"></path>
         </svg>
         {/* <p className="glitch-text" data-text="SENTRY TEST APP">SENTRY TEST APP</p>
@@ -95,7 +107,10 @@ function App() {
               <button data-button-size="sm" onClick={() => removeExtraDataRow(index)}>Del</button>
             </div>
           ))}
-          <button data-button-size="sm" onClick={addExtraDataRow}>Add</button>
+          <div className="button-row">
+            <button data-button-size="sm" onClick={addExtraDataRow}>Add</button>
+            <button data-button-size="sm" className="clear-button" onClick={clearAllData}>Clear</button>
+          </div>
         </div>
 
         <p>
